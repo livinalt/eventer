cc34c1833de2d0f0ba329b1312f87b34743b78b9
Jeremiah Samuel <livinalt@gmail.com>
2024-05-28 17:16:13 +0100

    initialising frontend

---
+++ ui/eventDapp/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <p className="read-the-docs text-blue-800">
+        Project EVENT DAPP
+      </p>
+    </>
+  )
+}
+
+export default App
